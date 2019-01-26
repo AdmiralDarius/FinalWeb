184e03634692e6ba28497c2afed80cb648d40e3c
shayan <yusefshayan@gmail.com>
2019-01-26 19:55:58 +0330

    model fix style fix league and all_league pages

--- games/views.py
+++ games/views.py
@@ -1,4 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from games import models
+
 
 # Create your views here.
 
+def all_leagues(request):
+    leagues = models.League.objects.order_by("start_date")
+    context = {
+        "leagues": leagues,
+    }
+    return render(request, "Shayan/All_leagues.html", context)
+
+
+def get_league(request, league_id):
+    league = get_object_or_404(models.League.objects, pk=league_id)
+    league_game = models.Game.objects.filter(leaguegame__in=league.leaguegame_set.all())
+    if league.is_football:
+        league_game = league_game.select_related('team1', 'team2', 'football_states').values(
+            'team1__name', 'team2__name', 'date', 'football_states__f_score',
+            'football_states__s_score', 'football_states__finished')
+    else:
+        league_game = league_game.select_related('team1', 'team2', 'basketball_states').values(
+            'team1__name', 'team2__name', 'date', 'basketball_states__f_score',
+            'basketball_states__s_score', 'basketball_states__finished')
+
+    league_game = models.Game.objects.filter(
+        leaguegame__in=league.leaguegame_set.all()).select_related('team1', 'team2',
+                                                                   'football_states').all()
+    league_team = league.leagueteam_set.all().values('wins', 'loses', 'team__name', 'draw',
+                                                     'goals_diff', 'goals_received',
+                                                     'goals_scored', 'score', 'team', 'game')
+
+    context = {
+        'league': league,
+        'league_game': league_game,
+        'league_team': league_team,
+    }
+    return render(request, "Shayan/leagues.html", context)
--- info/models.py
+++ info/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Player(models.Model):  # Darius
     age = models.IntegerField()
     name = models.CharField(max_length=100)
@@ -12,6 +13,7 @@ class Team(models.Model):  # Darius
     name = models.CharField(max_length=100)
     isfootball = models.BooleanField(default=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
 
 def user_directory_path(instance, filename):
     return '{0}/{1}'.format("news_pics", filename)
@@ -35,12 +37,10 @@ class RelateNewsTeam(models.Model):  # Darius
     date_added = models.DateTimeField(auto_now_add=True)
 
 
-
 class RelateNewsPlayer(models.Model):  # Darius
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     news = models.ForeignKey(News, on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
-
 
 
 class Tag(models.Model):  # Darius
