ffc5ba7bf238e6d8b704532061bb75638efe2102
AdmiralDarius <DariusSafavy@gmail.com>
2019-02-01 23:25:15 +0330

    cahnge

--- sfuser/urls.py
+++ sfuser/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('a_player/<int:id>', views.sfuser_player, name='like_player'),
     path('a_team/<int:id>', views.sfuser_team, name='like_team'),
     path('login/', views.darius_login, name='login'),
+    path('save_comment/<int:id>', views.save_comment, name='save_comment'),
     path('logout/', views.darius_logout, name='logout'),
     path('register/', views.user_register, name='register'),
 ]
